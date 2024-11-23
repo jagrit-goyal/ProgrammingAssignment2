d869d48c181a92a259e76ab87bcbc920f3857c2c
Jagrit Goyal <125178986+jagrit-goyal@users.noreply.github.com>
2024-11-23 22:38:38 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,42 +1,55 @@
-## Pair of functions that cache the inverse of a matrix
-## Usage: Pass the result of a makeCacheMatrix call to cacheSolve 
+## The makeCacheMatrix function creates a special "matrix" object that can cache its inverse.
+## This is useful for optimizing calculations, as the inverse of a matrix can be reused
+## without recalculating it if it hasn't changed.
 
-#' Util function that set the matrix and the inverse in an environment
-#' @param x an invertible matrix
-#' examples
-#' x = makeCacheMatrix(matrix(rnorm(9), 3, 3))
-#' x$set(matrix(rnorm(16), 4, 4))
 makeCacheMatrix <- function(x = matrix()) {
-  # todo error if x is not a matrix
-  inv <- NULL
+  s <- NULL  # 's' will store the cached inverse of the matrix, initially NULL.
+  
+  # The 'set' function updates the matrix with a new value and clears the cached inverse.
   set <- function(y) {
-    x <<- y
-    inv <<- NULL
+    x <<- y  # Assigns the new matrix to 'x' in the parent environment.
+    s <<- NULL  # Clears any previously cached inverse since the matrix has changed.
   }
+  
+  # The 'get' function simply returns the current matrix.
   get <- function() x
-  setinverse <- function(inverse) inv <<- inverse
-  getinverse <- function() inv
+  
+  # The 'setsolve' function stores the inverse of the matrix in the cache.
+  setsolve <- function(solve) s <<- solve
+  
+  # The 'getsolve' function retrieves the cached inverse, if it exists.
+  getsolve <- function() s
+  
+  # Returns a list of all four functions, giving external access to the matrix and cache.
   list(set = set, get = get,
-       setinverse = setinverse,
-       getinverse = getinverse)
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
-#' Compute and cache the inverse of a matrix
-#' @param x the result of a previous makeCacheMatrix call
-#' @param ... additional arguments to pass to solve function
-#' examples
-#' x = makeCacheMatrix(matrix(rnorm(9), 3, 3))
-#' cacheSolve(x)
+## The cacheSolve function calculates the inverse of the special "matrix" created by makeCacheMatrix.
+## If the inverse has already been calculated (and the matrix hasn't changed), then cacheSolve
+## retrieves the inverse from the cache to save computation time.
+
 cacheSolve <- function(x, ...) {
-  ## Return a matrix that is the inverse of 'x'
-  inv <- x$getinverse()
-  if(!is.null(inv)) {
-    message("getting cached matrix inverse")
-    return(inv)
+  # Retrieves any existing cached inverse.
+  s <- x$getsolve()
+  
+  # If the cached inverse exists, a message is shown and it is returned immediately.
+  if(!is.null(s)) {
+    message("getting inversed matrix")
+    return(s)
   }
+  
+  # If there is no cached inverse, the function gets the matrix.
   data <- x$get()
-  inv <- solve(data, ...)
-  x$setinverse(inv)
-  inv
+  
+  # Calculates the inverse of the matrix.
+  s <- solve(data, ...)
+  
+  # Caches the calculated inverse for future use.
+  x$setsolve(s)
+  
+  # Returns the newly calculated inverse.
+  s
 }
